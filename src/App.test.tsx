bce3eaa07c2352ce0b124db02d0498a542b93ddd
Ander Conal <ander.conal@plainconcepts.com>
2022-03-23 19:42:30 +0100

    Update Jest and Testing Library unit tests block description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import {fireEvent, render, screen} from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  // Arrange + Act
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+describe('Unit tests with Jest and Testing Library: Testing Library 101 TypeScript Workshop', () => {
+  test('renders learn react link', () => {
+    // Arrange + Act
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+
+    // Assert
+    expect(linkElement).toBeInTheDocument();
+  });
   
-  // Assert
-  expect(linkElement).toBeInTheDocument();
+  test('increments count on button click', () => {
+    // Arrange
+    render(<App />);
+    expect(screen.getByText(/you clicked 0 times/i)).toBeInTheDocument();
+
+    // Act
+    const clickMeButton = screen.getByRole('button', { name: /click me/i });
+    fireEvent.click(clickMeButton);
+
+    // Assert
+    expect(screen.queryByText(/you clicked 0 times/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/you clicked 1 times/i)).toBeInTheDocument();
+  });
 });
 
-test('increments count on button click', () => {
-  // Arrange
-  render(<App />);
-  expect(screen.getByText(/you clicked 0 times/i)).toBeInTheDocument();
-  
-  // Act
-  const clickMeButton = screen.getByRole('button', { name: /click me/i });
-  fireEvent.click(clickMeButton);
-  
-  // Assert
-  expect(screen.queryByText(/you clicked 0 times/i)).not.toBeInTheDocument();
-  expect(screen.getByText(/you clicked 1 times/i)).toBeInTheDocument();
-});

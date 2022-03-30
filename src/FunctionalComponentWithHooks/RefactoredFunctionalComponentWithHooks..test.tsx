2f229f4ac503bf660d091b85f8cdf7364948985a
Ander Conal <ander.conal@plainconcepts.com>
2022-03-30 20:05:30 +0200

    Format & lint

--- src/FunctionalComponentWithHooks/RefactoredFunctionalComponentWithHooks..test.tsx
+++ src/FunctionalComponentWithHooks/RefactoredFunctionalComponentWithHooks..test.tsx
@@ -1,19 +1,19 @@
-import {fireEvent, render, screen} from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import {RefactoredFunctionalComponentWithHooks} from './RefactoredFunctionalComponentWithHooks';
+import { RefactoredFunctionalComponentWithHooks } from './RefactoredFunctionalComponentWithHooks';
 
 describe('Unit tests with Jest and Testing Library: Testing Library 101 TypeScript Workshop', () => {
-    test('RefactoredFunctionalComponentWithHooks increments count on button click', () => {
-        // Arrange
-        render(<RefactoredFunctionalComponentWithHooks/>);
-        expect(screen.getByText(/you clicked 0 times/i)).toBeInTheDocument();
+  test('RefactoredFunctionalComponentWithHooks increments count on button click', () => {
+    // Arrange
+    render(<RefactoredFunctionalComponentWithHooks />);
+    expect(screen.getByText(/you clicked 0 times/i)).toBeInTheDocument();
 
-        // Act
-        const clickMeButton = screen.getByRole('button', {name: /click me/i});
-        fireEvent.click(clickMeButton);
+    // Act
+    const clickMeButton = screen.getByRole('button', { name: /click me/i });
+    fireEvent.click(clickMeButton);
 
-        // Assert
-        expect(screen.queryByText(/you clicked 0 times/i)).not.toBeInTheDocument();
-        expect(screen.getByText(/you clicked 1 times/i)).toBeInTheDocument();
-    });
+    // Assert
+    expect(screen.queryByText(/you clicked 0 times/i)).not.toBeInTheDocument();
+    expect(screen.getByText(/you clicked 1 times/i)).toBeInTheDocument();
+  });
 });
--- src/FunctionalComponentWithHooks/RefactoredFunctionalComponentWithHooks.tsx
+++ src/FunctionalComponentWithHooks/RefactoredFunctionalComponentWithHooks.tsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export function RefactoredFunctionalComponentWithHooks() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <h4>You clicked {count} times</h4>
-            <button onClick={() => setCount(count + 1)}>Click me</button>
-            <button onClick={() => setCount(count - 1)}>Added on refactor</button>
-        </>
-    );
+  return (
+    <>
+      <h4>You clicked {count} times</h4>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(count - 1)}>Added on refactor</button>
+    </>
+  );
 }

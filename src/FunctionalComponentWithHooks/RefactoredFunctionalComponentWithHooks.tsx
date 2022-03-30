7e29684eda9d30b9990ddb12e1b8b09be1f0935b
Ander Conal <ander.conal@plainconcepts.com>
2022-03-30 20:03:24 +0200

    Add option to decrease counter

--- src/FunctionalComponentWithHooks/RefactoredFunctionalComponentWithHooks.tsx
+++ src/FunctionalComponentWithHooks/RefactoredFunctionalComponentWithHooks.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 export function RefactoredFunctionalComponentWithHooks() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <>
-      <h4>You clicked {count} times</h4>
-      <button onClick={() => handleClick()}>Click me</button>
-    </>
-  );
+    return (
+        <>
+            <h4>You clicked {count} times</h4>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+            <button onClick={() => setCount(count - 1)}>Added on refactor</button>
+        </>
+    );
 }

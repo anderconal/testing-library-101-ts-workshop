9b0af361a0687a6111013ca7cfea2994a80806c5
Andrea Mariñosa <amarinosa@plainconcepts.com>
2022-04-04 08:10:07 +0200

    Init cypress

---
+++ cypress/integration/App.spec.ts
@@ -0,0 +1,21 @@
+describe('When load home page', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('increments count on button click', () => {
+    cy.visit('/');
+
+    // Arrange
+    cy.contains('You clicked 0 times').should('exist');
+
+    // Act
+    const clickMeButton = cy.get('button').contains('Click me');
+    clickMeButton.click();
+
+    // Assert
+    cy.contains('You clicked 0 times').should('not.exist');
+    cy.contains('You clicked 1 times').should('exist');
+
+  });
+});

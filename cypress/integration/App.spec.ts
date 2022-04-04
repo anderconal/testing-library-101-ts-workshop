1e6e5c32c97f315080479df0f1bf75c10a25a0fb
Ander Conal <ander.conal@plainconcepts.com>
2022-04-04 20:33:05 +0200

    Disable ESLint for Cypress tests

--- cypress/integration/App.spec.ts
+++ cypress/integration/App.spec.ts
@@ -1,31 +1,35 @@
 // @ts-ignore
 describe('End-to-end tests with Cypress: Testing Library 101 TypeScript Workshop', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
+    beforeEach(() => {
+        cy.visit('/');
+    });
 
-  it('Plain Cypress: increments count on button click', () => {
-    // Arrange
-    cy.contains('You clicked 0 times').should('exist');
+    it('Plain Cypress: increments count on button click', () => {
+        // Arrange
+        cy.contains('You clicked 0 times').should('exist');
 
-    // Act
-    const clickMeButton = cy.get('button').contains('Click me');
-    clickMeButton.click();
+        // Act
+        const clickMeButton = cy.get('button').contains('Click me');
+        clickMeButton.click();
 
-    // Assert
-    cy.contains('You clicked 0 times').should('not.exist');
-    cy.contains('You clicked 1 times').should('exist');
-  });
+        // Assert
+        cy.contains('You clicked 0 times').should('not.exist');
+        cy.contains('You clicked 1 times').should('exist');
+    });
 
-  it('Cypress with Testing Library: increments count on button click', () => {
-    // Arrange
-    cy.findByText(/you clicked 0 times/i).should('exist');
+    it('Cypress with Testing Library: increments count on button click', () => {
+        // Arrange
+        // eslint-disable-next-line
+        cy.findByText(/you clicked 0 times/i).should('exist');
 
-    // Act
-    cy.findByRole('button', { name: /click me/i }).click();
+        // Act
+        // eslint-disable-next-line
+        cy.findByRole('button', {name: /click me/i}).click();
 
-    // Assert
-    cy.findByText(/you clicked 0 times/i).should('not.exist');
-    cy.findByText(/you clicked 1 times/i).should('exist');
-  });
+        // Assert
+        // eslint-disable-next-line
+        cy.findByText(/you clicked 0 times/i).should('not.exist');
+        // eslint-disable-next-line
+        cy.findByText(/you clicked 1 times/i).should('exist');
+    });
 });

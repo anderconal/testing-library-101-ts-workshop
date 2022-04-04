acf24264cef0ebf97f50f0f4fb529ecc52d3cfe9
Ander Conal <ander.conal@plainconcepts.com>
2022-04-04 20:27:10 +0200

    Add Cypress with Testing Library test

--- cypress/integration/App.spec.ts
+++ cypress/integration/App.spec.ts
@@ -1,9 +1,10 @@
-describe('When load home page', () => {
-  before(() => {
+// @ts-ignore
+describe('End-to-end tests with Cypress: Testing Library 101 TypeScript Workshop', () => {
+  beforeEach(() => {
     cy.visit('/');
   });
 
-  it('increments count on button click', () => {
+  it('Plain Cypress: increments count on button click', () => {
     // Arrange
     cy.contains('You clicked 0 times').should('exist');
 
@@ -14,5 +15,17 @@ describe('When load home page', () => {
     // Assert
     cy.contains('You clicked 0 times').should('not.exist');
     cy.contains('You clicked 1 times').should('exist');
+  });
+
+  it('Cypress with Testing Library: increments count on button click', () => {
+    // Arrange
+    cy.findByText(/you clicked 0 times/i).should('exist');
+
+    // Act
+    cy.findByRole('button', { name: /click me/i }).click();
+
+    // Assert
+    cy.findByText(/you clicked 0 times/i).should('not.exist');
+    cy.findByText(/you clicked 1 times/i).should('exist');
   });
 });
